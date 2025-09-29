b0493621c459d07714925d962c1242c47e8a2474
milado98 <chimaemma04@gmail.com>
2025-09-29 07:16:08 +0100

    fixed import case mismatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import PostCard from "./components/PostCard";
 import Sidebar from "./components/Sidebar";
 import { posts } from "./data";
